cec1c70abedce62e0a13c42ea8b075c91784d17c
Amanda Veiga <avsantos573@gmail.com>
2024-05-26 19:21:23 -0300

    adjusting the pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    navigate('home'); // carrega a pagina inicial por padrão
-});
-
-function navigate(page) {
-    fetch(page + '.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new error('Network response was not ok' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => {
-            console.log('There has been a problem with your fetch operation:', error);
-        });
+function navigateTo(page) {
+    window.location.href = page;
 }
