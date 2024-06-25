34581d3e9ef532a85e88b5accf5bab5488b4b805
Amanda Veiga <avsantos573@gmail.com>
2024-06-25 19:48:27 -0300

    included responsive

--- catalogo/estilosGeneros/autoajuda.js
+++ catalogo/estilosGeneros/autoajuda.js
@@ -1,45 +1,68 @@
 $(document).ready(function(){
-  $('.carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      prevArrow: '<button type="button" class="prev"><i class="bi bi-arrow-left-circle-fill"></i></button>',
-      nextArrow: '<button type="button" class="next"><i class="bi bi-arrow-right-circle-fill"></i></button>',
-      responsive: [
-          {
-              breakpoint: 1024,
-              settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 4,
-                  infinite: true,
-                  dots: true
-              }
-          },
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
-                  dots: true
-              }
-          },
-          {
-              breakpoint: 600,
-              settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-              }
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-              }
-          }
-      ]
-  });
+    function aplicarEstilosBotoes() {
+        $('.btn#botaoComprar').css({
+            'background-color': '#000',
+            'border': '#000',
+            'color': '#fff',
+            'font-weight': 'bold',
+            'font-size': '20px',
+            'margin-top': '9px',
+            'height': '50px',
+            'width': '170px'
+        });
+
+        $('.btn#botaoComprar:hover').css({
+            'background-color': '#fceabb',
+            'color': 'black',
+            'font-weight': 'bold'
+        });
+    }
+
+    aplicarEstilosBotoes();
+
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="prev"><i class="bi bi-arrow-left-circle-fill"></i></button>',
+        nextArrow: '<button type="button" class="next"><i class="bi bi-arrow-right-circle-fill"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    }).on('afterChange', function(event, slick, currentSlide){
+        aplicarEstilosBotoes();
+    });
 });
